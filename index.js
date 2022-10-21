f15309fbf38127b294df966c73b950e2cd772b7f
deadseal001 <liwenbo4936@gmail.com>
2022-10-20 22:12:36 -0500

    function done

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+// const generatePage= require('./src/generatePage');
 
 const managerArr=[];
 const engineerArry=[];
 const internArry=[];
 
-function addMemeber() {
-    inquirer
-        .prompt({
-            type: 'list',
-            name:'title',
-            message: 'Please Choose the Title of the Employee that You Want to Add:',
-            choices: ['Manager', 'Engineer', 'Intern']
-            }
-        )
-        .then((Input)=>{
-            switch(Input.title){
-                case 'Manager':
-                    //call add manager function
-                    addManager();
-                    break;
-                case 'Engineer':
-                    //call add engineer function
-                    addEngineer();
-                    break;
-                case 'Intern':
-                    //call add Intern function
-                    addIntern();
-                    break;
-                default:
-                    break;
-            }
-            return (managerArr,engineerArry,internArry);
-        })    
-};
+// function addMemeber() {
+//     return new Promise (resolve=>{
+//     inquirer
+//         .prompt({
+//             type: 'list',
+//             name:'choose',
+//             message: 'Please Choose the What You Want to Do:',
+//             choices: ['Add Engineer', 'Add Intern','Finish building my team']
+//             }
+//         )
+//         .then((Input)=>{
+//             switch(Input.choose){
+//                 case 'Add Engineer':
+//                     //call add engineer function
+//                     return (addEngineer());
+//                 case 'Add Intern':
+//                     //call add Intern function
+//                     return (addIntern());
+//                 default:
+//                     return generatePage(managerArr, engineerArry,internArry);
+//             }
+//         })
+//     })
+
+// };
 
 function addManager() {
     inquirer
@@ -89,19 +87,26 @@ function addManager() {
                 } 
             },
             {
-                type: 'confirm',
-                name: 'addmore',
-                message: 'Would You Like to Another Employee?',
-                default: true
+                type: 'list',
+                name:'choose',
+                message: 'Please Choose the What You Want to Do:',
+                choices: ['Add Engineer', 'Add Intern','Finish building my team']
             }
         ])
         .then((Input)=>{
-            managerArr.push(new Manager(Input.name, Input.id, Input.email, Input.officeNumber));
-            return Input.addmore;
+            managerArr.push(new Manager(Input.name, Input.id, Input.email, Input.officeNumber));  
+            return Input;
         })
-        .then((addmore)=>{
-            if (addmore) {
-                new addMemeber();
+        .then((Input)=>{
+            switch(Input.choose){
+                case 'Add Engineer':
+                    //call add engineer function
+                    return (addEngineer());
+                case 'Add Intern':
+                    //call add Intern function
+                    return (addIntern());
+                default:
+                    return generatePage(managerArr, engineerArry,internArry);
             }
         })
 }
@@ -158,19 +163,26 @@ function addEngineer() {
                 } 
             },
             {
-                type: 'confirm',
-                name: 'addmore',
-                message: 'Would You Like to Another Employee?',
-                default: true
+                type: 'list',
+                name:'choose',
+                message: 'Please Choose the What You Want to Do:',
+                choices: ['Add Engineer', 'Add Intern','Finish building my team']
             }
         ])
         .then((Input)=>{
             engineerArry.push(new Engineer(Input.name, Input.id, Input.email, Input.github));
-            return Input.addmore;
+            return Input;
         })
-        .then((addmore)=>{
-            if (addmore) {
-                new addMemeber();
+        .then((Input)=>{
+            switch(Input.choose){
+                case 'Add Engineer':
+                    //call add engineer function
+                    return (addEngineer());
+                case 'Add Intern':
+                    //call add Intern function
+                    return (addIntern());
+                default:
+                    return generatePage(managerArr, engineerArry,internArry);
             }
         })
 }
@@ -227,36 +239,173 @@ function addIntern() {
                 } 
             },
             {
-                type: 'confirm',
-                name: 'addmore',
-                message: 'Would You Like to Another Employee?',
-                default: true
+                type: 'list',
+                name:'choose',
+                message: 'Please Choose the What You Want to Do:',
+                choices: ['Add Engineer', 'Add Intern','Finish building my team']
             }
         ])
         .then((Input)=>{
             internArry.push(new Intern(Input.name, Input.id, Input.email, Input.school));
-            return Input.addmore;
+            return Input;
         })
-        .then((addmore)=>{
-            if (addmore) {
-                new addMemeber();
+        .then((Input)=>{
+            switch(Input.choose){
+                case 'Add Engineer':
+                    //call add engineer function
+                    return (addEngineer());
+                case 'Add Intern':
+                    //call add Intern function
+                    return (addIntern());
+                default:
+                    return generatePage(managerArr, engineerArry,internArry);
             }
         })
 }
 
+function generateCards(a,b,c){
+    
+    return`
+      ${a.map((element)=>{
+        // console.log("a");
+        return`
+          <card class="mangerCard col-2 shadow-sm">
+            <div  class="text-dark bg-secondary">
+              ${element.name}<br>
+              <i class="fa fa-mug-hot"> Manager</i>
+            </div>  
+            <div class="col-12 flex justify-content-center">
+              <div class="col-10 my-4 text-dark">
+                <div>
+                ${element.getID()}
+                </div>
+                <div>
+                <a href="mailto:${element.email}">${element.getEmail()}</a>
+                </div>
+                <div>
+                ${element.getOfficeNumber()}
+                </div>
+              </div>
+            </div>
+          </card>
+        `
+      })
+      .join('')
+      }
+      ${b.map((element)=>{
+        return` 
+          <card class="mangerCard col-2 text-dark bg-success">
+            <div class="text-dark bg-success">
+              ${element.name}<br>
+              <i class="fa fa-glasses"> Engineer</i>
+            </div>
+            <div class="col-12 flex justify-content-center">
+              <div class="col-10 my-4 text-dark">
+                <div>
+                ${element.getID()}
+                </div>
+                <div>
+                <a href="mailto:${element.email}">${element.getEmail()}</a>
+                </div>
+                <div>
+                <a href="https://github.com/${element.github}" target="_blank">${element.getGithub()}</a>
+                </div>
+              </div>
+            </div>
+          </card>
+        `
+      })
+      .join('')
+      } 
+      ${c.map((element)=>{
+        return`
+          <card class="mangerCard col-2 text-dark bg-warning">
+            <div class="text-dark bg-warning">
+              ${element.name}<br>
+              <i class="fa fa-graduation-cap"> Intern</i>
+            </div>
+            <div class="col-12 flex justify-content-center">
+              <div class="col-10 my-4 text-dark">
+                <div>
+                ${element.getID()}
+                </div>
+                <div>
+                <a href="mailto:${element.email}">${element.getEmail()}</a>
+                </div>
+                <div>
+                ${element.getSchool()}
+                </div>
+              </div>
+            </div>
+          </card>
+        `
+      })
+      .join('')
+      } 
+    `
+}
+  
+function generatePage(a,b,c) {
+//console.log(a,b,c);
+    let page= `
+<!DOCTYPE html>
+<html lang="en">
 
-new addMemeber();
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Profile Manager</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="../src/style.css">
+</head>
 
+<body>
+    <header>
+    <div class="container bg-red flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-center text-dark bg-gradient bg-primary py-2 px-3">My Team</h1>
+    </header>
+    <main class="container flex my-5">
+    ${generateCards(a,b,c)}
+    </main>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
+    
+    `
+    writeToFile(page);
+}
+
+function writeToFile(data){
+    console.log("writefile")
+    return new Promise((resolve,regject)=>{
+        fs.writeFile('./dist/index.html',data, err=>{
+            if (err) {
+                reject (err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+}
 
 function init(){
-    addMemeber()
-    .then((a,b,c) =>{
-        return generatePage(a,b,c);
-    } )
-    .then(page=>{
-        return writeToFile(page);
-    })
-    .catch(err=>{
-        console.log(err);
-    })
+    addManager()
+    // .then(page=>{
+    //     console.log(page);
+    //     return writeToFile(page);
+    // })
+    // .catch(err=>{
+    //     console.log(err);
+    // })
 };
+
+
+init();
+// addMemeber();
